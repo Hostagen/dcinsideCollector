348abdb63dcfef33bb298490dc3be5b3c177dadf
Hostagen <68284806+Hostagen@users.noreply.github.com>
2022-03-28 19:42:06 +0900

    add Main Form.

---
+++ DCinside-Collector/src/Main.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace dcinside_collector
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.LoadGallButton1 = new MetroFramework.Controls.MetroButton();
+            this.disableAnnounCheckBox = new System.Windows.Forms.CheckBox();
+            this.articleDataGrid = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.author = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.articleDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadGallButton1
+            // 
+            resources.ApplyResources(this.LoadGallButton1, "LoadGallButton1");
+            this.LoadGallButton1.Name = "LoadGallButton1";
+            this.LoadGallButton1.UseSelectable = true;
+            this.LoadGallButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // disableAnnounCheckBox
+            // 
+            resources.ApplyResources(this.disableAnnounCheckBox, "disableAnnounCheckBox");
+            this.disableAnnounCheckBox.Checked = true;
+            this.disableAnnounCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.disableAnnounCheckBox.Name = "disableAnnounCheckBox";
+            this.disableAnnounCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // articleDataGrid
+            // 
+            this.articleDataGrid.AllowUserToAddRows = false;
+            this.articleDataGrid.AllowUserToResizeColumns = false;
+            this.articleDataGrid.AllowUserToResizeRows = false;
+            this.articleDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.articleDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.title,
+            this.author,
+            this.date});
+            resources.ApplyResources(this.articleDataGrid, "articleDataGrid");
+            this.articleDataGrid.Name = "articleDataGrid";
+            this.articleDataGrid.ReadOnly = true;
+            this.articleDataGrid.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.articleDataGrid.RowHeadersVisible = false;
+            this.articleDataGrid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.articleDataGrid.RowTemplate.Height = 23;
+            this.articleDataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            // 
+            // Column1
+            // 
+            this.Column1.FillWeight = 30F;
+            resources.ApplyResources(this.Column1, "Column1");
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // title
+            // 
+            this.title.FillWeight = 111.2835F;
+            resources.ApplyResources(this.title, "title");
+            this.title.Name = "title";
+            this.title.ReadOnly = true;
+            // 
+            // author
+            // 
+            resources.ApplyResources(this.author, "author");
+            this.author.Name = "author";
+            this.author.ReadOnly = true;
+            // 
+            // date
+            // 
+            this.date.FillWeight = 111.2835F;
+            resources.ApplyResources(this.date, "date");
+            this.date.Name = "date";
+            this.date.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.articleDataGrid);
+            this.Controls.Add(this.disableAnnounCheckBox);
+            this.Controls.Add(this.LoadGallButton1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.articleDataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MetroFramework.Controls.MetroButton LoadGallButton1;
+        private System.Windows.Forms.CheckBox disableAnnounCheckBox;
+        private System.Windows.Forms.DataGridView articleDataGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn author;
+        private System.Windows.Forms.DataGridViewTextBoxColumn date;
+    }
+}
+
---
+++ DCinside-Collector/src/Main.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace dcinside_collector
+{
+    public partial class Main : MetroFramework.Forms.MetroForm
+    {
+
+        public string gallURL = "";
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileChecker checkForm = new FileChecker();
+            checkForm.ShowDialog();
+        }
+
+        private readonly Regex TIME_REGEX = new Regex(@"[0-9]{2}:[0-9]{2}");
+        private readonly Regex YMD_REGEX = new Regex(@"[0-9]{2}.[0-9]{2}.[0-9]{2}");
+
+        public DateTime ConverToDate(string datetime)
+        {
+
+            if (TIME_REGEX.IsMatch(datetime))
+            {
+                return DateTime.ParseExact(datetime, "HH:mm", null);
+            }
+
+            if (YMD_REGEX.IsMatch(datetime))
+            {
+                return DateTime.ParseExact(datetime, "yy.MM.dd", null);
+            }
+
+            return DateTime.ParseExact(datetime, "MM.dd", null);
+        }
+
+        private List<Article> getArticles(HtmlAgilityPack.HtmlDocument doc)
+        {
+            HtmlNodeCollection articleNodes = doc.DocumentNode.SelectNodes("//tbody/tr");
+
+            List<Article> results = new List<Article>();
+
+            foreach (HtmlNode node in articleNodes)
+            {
+                HtmlNode articleSubjectNode = node.SelectSingleNode("./td[@class='gall_subject']");
+                string articleSubject = articleSubjectNode.InnerText;
+
+                HtmlNode articleSubjectP = articleSubjectNode.SelectSingleNode("./p");
+
+                if (articleSubjectP != null)
+                {
+                    articleSubject = articleSubjectP.InnerText;
+                }
+
+                if (articleSubject == "설문" || articleSubject == "공지" && disableAnnounCheckBox.Checked)
+                {
+                    continue;
+                }
+
+                HtmlNode articleNameNode = node.SelectSingleNode("td/a");
+                string articleName = articleNameNode.InnerText;
+
+                //HtmlNode authorName = node.SelectSingleNode()
+
+                HtmlNode dateNode = node.SelectSingleNode("./td[@class='gall_date']");
+                DateTime articleDate = ConverToDate(dateNode.InnerText);
+
+                Article result = new Article(
+                    subject: articleSubject,
+                    title: articleName,
+                    owner: "ㅇㅇ",
+                    datetime: articleDate
+                );
+
+                results.Add(result);
+            }
+
+            return results;
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            GallerySelecter newFrom = new GallerySelecter();
+            newFrom.ShowDialog();
+            gallURL = newFrom.gallUrl;
+
+            if (gallURL == "" || gallURL == null)
+            {
+                return;
+            }
+
+            string baseURL = "{0}&page={1}";
+
+            articleDataGrid.Rows.Clear();
+
+            HtmlWeb web = new HtmlWeb();
+
+            DateTime getRangeDate = newFrom.GetRange.Value;
+
+            int i;
+
+            for (i = 1; ; i++)
+            {
+                Console.WriteLine(i);
+                string url = String.Format(baseURL, gallURL, i.ToString());
+                HtmlAgilityPack.HtmlDocument htmlDoc = web.Load(url);
+
+                if (web.ResponseUri.ToString() != url)
+                {
+                    break;
+                }
+
+                List<Article> articles = getArticles(htmlDoc);
+                Article lastArticle = articles.Last();
+
+                if (
+                    !articles.Any() || 
+                    lastArticle.Created_at.Year != getRangeDate.Year || 
+                    lastArticle.Created_at.Month != getRangeDate.Month
+                    )
+                {
+                    break;
+                }
+
+                foreach (Article article in articles)
+                {
+                    articleDataGrid.Rows.Add(article.Category, article.Name, article.author, article.Created_at);
+                }
+            }
+        }
+    }
+    class Article
+    {
+        public string Category;
+        public string Name;
+        public string author;
+        public DateTime Created_at;
+
+        public Article(string subject, string title, string owner, DateTime datetime)
+        {
+            Category = subject;
+            Name = title;
+            author = owner;
+            Created_at = datetime;
+        }
+    }
+}
