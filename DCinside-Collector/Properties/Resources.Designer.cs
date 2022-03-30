4a020e1e42dba77ab57091ef8a04fb63c25b6402
Hostagen <68284806+Hostagen@users.noreply.github.com>
2022-03-30 22:27:01 +0900

    edit Resources.Designer.cs

--- DCinside-Collector/Properties/Resources.Designer.cs
+++ DCinside-Collector/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace test.DCinside_Collector.Properties {
+namespace dcinsideCollector.DCinside_Collector.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace test.DCinside_Collector.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("test.DCinside_Collector.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("dcinsideCollector.DCinside_Collector.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
